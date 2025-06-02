c42974dda45cf524fd4fb5355a5a86fc6f9a6d9e
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2025-06-02 15:44:44 -0600

    Agregué un go y creé tabla de mantenimiento

--- ALQUIFACIL-DB/01_Estructura/mantenimiento_tablas.sql
+++ ALQUIFACIL-DB/01_Estructura/mantenimiento_tablas.sql
@@ -1,1 +1,28 @@
 -- TABLAS DEL FILEGROUP MANTENIMIENTO
+
+use alquifacil
+go
+create table Mantenimiento(
+	Id_Mantenimiento int not null,
+	Fecha_Mantenimiento datetime not null,
+	Persona_Responsable varchar(50) not null,
+	Observaciones varchar(256) null,
+	Costo money not null,
+	Modalidad_Servicio varchar(50) null,
+	Id_Herramienta int null
+)
+on MANTENIMIENTO
+go
+
+
+use alquifacil
+go
+alter table Mantenimiento
+add constraint PK_Mantenimiento_Id
+primary key (Id_Mantenimiento)
+go
+
+
+exec sp_help Mantenimiento
+go
+
