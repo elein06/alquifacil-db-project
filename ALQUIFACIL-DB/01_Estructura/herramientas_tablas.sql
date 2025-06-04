a6c1bad024972e398459aebb2d69d92a05255587
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2025-06-03 23:24:36 -0600

    Creación de la tabla Kit, y la tabla intermedia Kit-Herramienta, con sus respectivas primary key y foreign key.

--- ALQUIFACIL-DB/01_Estructura/herramientas_tablas.sql
+++ ALQUIFACIL-DB/01_Estructura/herramientas_tablas.sql
@@ -26,3 +26,78 @@ go
 
 exec sp_help Herramienta
 go
+
+
+
+--Creaci�n tabla Kit--
+use ALQUIFACIL
+GO
+
+CREATE TABLE Kit
+(
+codigo_Kit INT NOT NULL,
+nombre VARCHAR(50) NOT NULL,
+tarifa_Diaria_Especial MONEY NOT NULL,
+num_Contrato INT NOT NULL,
+id_Categoria INT NOT NULL
+)
+On HERRAMIENTAS
+GO
+
+--Agrego llave primaria a Kit--
+use ALQUIFACIL
+GO
+alter table Kit
+add constraint PK_Kit_codigo_Kit
+primary key (codigo_Kit)
+GO
+
+exec sp_help Kit
+go
+
+
+
+--Creaci�n tabla intermedia Kit-Herramienta--
+use ALQUIFACIL
+GO
+
+CREATE TABLE KitHerramienta
+(
+id_KitHerramienta INT NOT NULL,
+codigo_Kit INT NOT NULL,
+Id_Herramienta INT NOT NULL,
+cantidad_Herramientas INT NOT NULL
+)
+ON HERRAMIENTAS
+GO
+
+--Agrego llave primaria a KitHerramienta--
+
+use ALQUIFACIL
+GO
+alter table KitHerramienta
+add constraint PK_KitHerramienta_id_KitHerramienta
+primary key (id_KitHerramienta)
+GO
+
+
+
+-- Agrego llaves foráneas a la tabla KitHerramienta
+use ALQUIFACIL
+GO
+alter table KitHerramienta
+add constraint FK_KitHerramienta_codigo_Kit
+Foreign key (codigo_Kit)
+REFERENCES Kit(codigo_Kit)
+GO
+
+use ALQUIFACIL
+GO
+alter table KitHerramienta
+add constraint FK_KitHerramienta_Id_Herramienta
+Foreign key (Id_Herramienta)
+REFERENCES Herramienta(Id_Herramienta)
+GO
+
+exec sp_help KitHerramienta
+go
