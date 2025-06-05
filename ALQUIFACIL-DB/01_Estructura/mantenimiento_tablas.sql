4a4e41d91a15d2eaaa72410400e800c0e9adc8e5
elein06 <rodriguezrayeli06@gmail.com>
2025-06-05 15:29:41 -0600

    tabla mantenimiento corregida, tabla TipoMantenimiento lista

--- ALQUIFACIL-DB/01_Estructura/mantenimiento_tablas.sql
+++ ALQUIFACIL-DB/01_Estructura/mantenimiento_tablas.sql
@@ -5,11 +5,12 @@ go
 create table Mantenimiento
 (
 	Id_Mantenimiento int not null,
-	Fecha_Mantenimiento datetime not null,
-	Id_Persona_Responsable int not null,
-	Observaciones varchar(256) null,
 	Costo money not null,
+	Fecha_Mantenimiento datetime not null,
 	Modalidad_Servicio varchar(50) null,
+	Observaciones varchar(256) null,
+	Id_Tipo_Mantenimiento int not null,
+	Id_Persona_Responsable int not null,
 	Id_Herramienta int null
 )
 on MANTENIMIENTO
@@ -56,3 +57,33 @@ go
 
 exec sp_help Persona_Responsable
 go
+
+
+use alquifacil
+go
+create table tipo_mantenimiento
+(
+	Id_Tipo_Mantenimiento int not null,
+	nombre_tipo_mantenimiento varchar(50) not null
+)
+on MANTENIMIENTO
+go
+
+use alquifacil
+go
+alter table tipo_mantenimiento
+add constraint PK_Tipo_Mantenimiento_Id
+primary key (Id_Tipo_Mantenimiento)
+go
+
+use alquifacil
+go
+alter table Mantenimiento
+add constraint FK_Mantenimiento_TipoMantenimiento
+foreign key (Id_Tipo_Mantenimiento)
+references tipo_mantenimiento(Id_Tipo_Mantenimiento);
+go
+
+exec sp_help tipo_mantenimiento
+go
+
