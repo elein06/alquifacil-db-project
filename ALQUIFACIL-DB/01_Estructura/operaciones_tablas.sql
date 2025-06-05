b135ef927abadd25c4a221d318833b271b91b674
elein06 <rodriguezrayeli06@gmail.com>
2025-06-05 15:41:25 -0600

    cambio nombre de tabla 'operaciones' a 'alquiler' y creaciion de FK

--- ALQUIFACIL-DB/01_Estructura/operaciones_tablas.sql
+++ ALQUIFACIL-DB/01_Estructura/operaciones_tablas.sql
@@ -2,24 +2,34 @@
 
 use alquifacil
 go
-create table Operaciones(
+create table Alquiler
+(
 	num_Contrato int not null,
 	cliente_Asociado varchar(50) not null,
 	fecha_Inicio date not null,
 	fecha_Dev date not null,
 	tarifa_Total_Diaria	money not null,
 	deposito_Garantia money not null,
-	estado_Contrato int not null
+	estado_Contrato int not null,
+	Id_cliente int not null
 )
 on OPERACIONES
 go
 
 use alquifacil
 go
-alter table Operaciones
+alter table alquiler
 add constraint PK_num_Contrato
 primary key (num_Contrato)
 go
 
-exec sp_help Operaciones
+use alquifacil
+go
+alter table alquiler
+add constraint FK_Alquiler_IdCliente
+foreign key (Id_cliente)
+references cliente(id_cliente)
+go
+
+exec sp_help alquiler
 go
