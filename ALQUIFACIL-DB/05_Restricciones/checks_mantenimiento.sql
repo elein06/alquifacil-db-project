88937f87cddefdbb98264ca6d61566bf13e7ab49
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2025-06-06 15:42:23 -0600

    Checks de mantenimiento y persona responsable

--- ALQUIFACIL-DB/05_Restricciones/checks_mantenimiento.sql
+++ ALQUIFACIL-DB/05_Restricciones/checks_mantenimiento.sql
@@ -1,1 +1,55 @@
 -- Checks de filegroup mantenimietno 
+
+
+--			TABLA MANTENIMIENTO
+-- Costo no puede ser negativo
+use ALQUIFACIL
+go
+ALTER TABLE Mantenimiento
+ADD CONSTRAINT CK_Mantenimiento_Costo_Positive
+CHECK (Costo >= 0);
+go
+
+-- Fecha no puede ser futura
+use ALQUIFACIL
+go
+ALTER TABLE Mantenimiento
+ADD CONSTRAINT CK_Mantenimiento_Validar_Fecha
+CHECK (Fecha_Mantenimiento <= GETDATE());
+go
+
+-- Modalidad servicio: interno o externo
+use ALQUIFACIL
+go
+ALTER TABLE Mantenimiento
+ADD CONSTRAINT CK_Mantenimiento_Validar_Modalidad
+CHECK (Modalidad_Servicio IN ('Interno', 'Externo'));
+go
+
+
+
+--			TABLA PERSONA RESPONSABLE}
+
+-- Validar que nombre y apellidos no sean nulos
+use ALQUIFACIL
+go
+ALTER TABLE Persona_Responsable
+ADD CONSTRAINT CK_PersonaResponsable_Validar_Nombre
+CHECK (LTRIM(RTRIM(nombre)) <> '');
+go
+
+use ALQUIFACIL
+go
+ALTER TABLE Persona_Responsable
+ADD CONSTRAINT CK_PersonaResponsable_Validar_Apellido1
+CHECK (LTRIM(RTRIM(apellido1)) <> '');
+go
+
+use ALQUIFACIL
+go
+ALTER TABLE Persona_Responsable
+ADD CONSTRAINT CK_PersonaResponsable_Validar_Apellido2
+CHECK (LTRIM(RTRIM(apellido2)) <> '');
+go
+
+--			TABLA TIPO DE MANTENIMIENTO
