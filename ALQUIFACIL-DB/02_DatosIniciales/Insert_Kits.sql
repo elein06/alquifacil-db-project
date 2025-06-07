4b74a61a0ea5f8542bf9fd478792a0a68243e03b
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2025-06-07 00:06:57 -0600

    se agregó el insert y procedimiento almacenado de kit

--- ALQUIFACIL-DB/02_DatosIniciales/Insert_Kits.sql
+++ ALQUIFACIL-DB/02_DatosIniciales/Insert_Kits.sql
@@ -1,2 +1,35 @@
 -- Insert Kits
 -- Proyecto ALQUIFÁCIL
+
+--Procedimiento almacenado para agregar un kit 
+
+use ALQUIFACIL
+go
+
+create procedure sp_IngresarKit
+(
+    @_codigo_Kit int,
+    @_nombre varchar(50),
+    @_tarifa_Diaria_Especial money,
+    @_num_Contrato int,
+    @_id_Categoria int)
+AS
+    insert into Kit (codigo_Kit, nombre, tarifa_Diaria_Especial, num_Contrato, id_Categoria)
+    values (@_codigo_Kit, @_nombre, @_tarifa_Diaria_Especial, @_num_Contrato, @_id_Categoria)
+    PRINT 'EL KIT SE HA REGISTRADO CORRECTAMENTE'
+    go
+
+
+    exec sp_IngresarKit '1001', 'Kit de Jardinería', 15000, 1, 1
+    go
+
+    exec sp_IngresarKit '1002', 'Kit de Construcción', 20000, 2, 2
+    go
+
+    exec sp_IngresarKit '1003', 'Kit de Bricolaje', 12000, 3, 3
+    go
+
+    select * from Kit
+    go
+
+
