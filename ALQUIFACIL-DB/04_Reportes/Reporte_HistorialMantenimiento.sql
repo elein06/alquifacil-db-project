398da86a303691dad5768473d48c5abd0b60ef67
Daniel <161152546+SoyMaloLoSeShh@users.noreply.github.com>
2025-06-07 17:32:19 -0600

    Update Reporte_HistorialMantenimiento.sql

--- ALQUIFACIL-DB/04_Reportes/Reporte_HistorialMantenimiento.sql
+++ ALQUIFACIL-DB/04_Reportes/Reporte_HistorialMantenimiento.sql
@@ -1,2 +1,12 @@
--- Reporte HistorialMantenimiento
--- Proyecto ALQUIFÁCIL
+SELECT
+  Id_Herramienta,
+  COUNT(Id_Herramienta) AS Cantidad_Mantenimientos
+FROM
+  Mantenimiento
+WHERE
+  Id_Herramienta IS NOT NULL
+GROUP BY
+  Id_Herramienta
+ORDER BY
+  Cantidad_Mantenimientos DESC;
+GO
