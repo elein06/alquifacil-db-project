fb1e24e39beb5720b83b8883b37bd7682ede59f8
Daniel <161152546+SoyMaloLoSeShh@users.noreply.github.com>
2025-06-07 05:33:24 -0600

    Update checks_devoluciones.sql

--- ALQUIFACIL-DB/05_Restricciones/checks_devoluciones.sql
+++ ALQUIFACIL-DB/05_Restricciones/checks_devoluciones.sql
@@ -11,3 +11,12 @@ ALTER TABLE Devolucion
 ADD CONSTRAINT CK_Devolucion_CargosAtrasoPositivos
 CHECK (cargos_Por_Dia_Atraso IS NULL OR cargos_Por_Dia_Atraso >= 0);
 GO
+
+USE ALQUIFACIL
+GO
+
+-- CHECK para validar que la cantidad de herramientas sea mayor a 0 (si no es NULL)
+ALTER TABLE DevolucionHerramienta
+ADD CONSTRAINT CK_DevolucionHerramienta_CantidadPositiva
+CHECK (cantidad_Herramientas IS NULL OR cantidad_Herramientas > 0)
+GO
