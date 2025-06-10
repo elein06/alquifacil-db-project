fe704a42f18569afc6da0aa959dc167cc27fdb3d
Daniel <161152546+SoyMaloLoSeShh@users.noreply.github.com>
2025-06-10 15:41:38 -0600

    Update Insert_Alquileres.sql

--- ALQUIFACIL-DB/02_DatosIniciales/Insert_Alquileres.sql
+++ ALQUIFACIL-DB/02_DatosIniciales/Insert_Alquileres.sql
@@ -10,6 +10,7 @@ CREATE OR ALTER PROCEDURE sp_RegistrarAlquileresConHerramientas
     @_fecha_Dev DATE,
     @_tarifa_Total_Diaria MONEY,
     @_deposito_Garantia MONEY,
+	@_costo_alquiler money,
     @_estado_Contrato VARCHAR(50),
     @_Id_cliente INT,
     @_id_Herramienta INT,
@@ -59,8 +60,8 @@ BEGIN
     END
 
     -- Insertar contrato de alquiler
-    INSERT INTO Alquiler(fecha_Inicio, fecha_Dev, tarifa_Total_Diaria, deposito_Garantia, estado_Contrato, Id_cliente)
-    VALUES (@_fecha_Inicio, @_fecha_Dev, @_tarifa_Total_Diaria, @_deposito_Garantia, @_estado_Contrato, @_Id_cliente);
+    INSERT INTO Alquiler(fecha_Inicio, fecha_Dev, tarifa_Total_Diaria, deposito_Garantia, costo_alquiler, estado_Contrato, Id_cliente)
+    VALUES (@_fecha_Inicio, @_fecha_Dev, @_tarifa_Total_Diaria, @_deposito_Garantia, @_costo_alquiler, @_estado_Contrato, @_Id_cliente);
 
     SET @nuevoIdAlquiler = SCOPE_IDENTITY();
 
@@ -80,14 +81,17 @@ BEGIN
 END;
 GO
 
---registrar un alquiler de herramienta
+select * from Herramienta
+
+
 exec sp_RegistrarAlquileresConHerramientas
     @_fecha_Inicio = '2025-07-24',
     @_fecha_Dev = '2025-09-07',
     @_tarifa_Total_Diaria = 20000,
-    @_deposito_Garantia = 15000,
+    @_deposito_Garantia = 5000,
+	@_costo_alquiler = 15000,
     @_estado_Contrato = 'Activo',
-    @_Id_cliente = 1,
+    @_Id_cliente = 2,
 	@_id_Herramienta = 16,
 	@_cantidadHerramientas = 3
 
@@ -102,6 +106,7 @@ create or alter procedure sp_RegistrarAlquileresConKits
     @_fecha_Dev date,
     @_tarifa_Total_Diaria money,
     @_deposito_Garantia money,
+	@_costo_alquiler money,
     @_estado_Contrato varchar(50),
     @_Id_cliente int,
 	-- kit
@@ -173,8 +178,8 @@ begin
     END
 
     -- Insertar el alquiler (ID autogenerado)
-    insert into Alquiler(fecha_Inicio, fecha_Dev, tarifa_Total_Diaria, deposito_Garantia, estado_Contrato, Id_cliente)
-    values (@_fecha_Inicio, @_fecha_Dev, @_tarifa_Total_Diaria, @_deposito_Garantia, @_estado_Contrato, @_Id_cliente);
+    insert into Alquiler(fecha_Inicio, fecha_Dev, tarifa_Total_Diaria, deposito_Garantia, costo_alquiler, estado_Contrato, Id_cliente)
+    values (@_fecha_Inicio, @_fecha_Dev, @_tarifa_Total_Diaria, @_deposito_Garantia, @_costo_alquiler, @_estado_Contrato, @_Id_cliente);
 
     -- Obtener el nuevo ID de alquiler
     set @nuevoIdAlquiler = SCOPE_IDENTITY();
@@ -198,15 +203,15 @@ begin
 end
 go
 
---registrar un alquiler de un kit 
 exec sp_RegistrarAlquileresConKits
     @_fecha_Inicio = '2025-07-24',
     @_fecha_Dev = '2025-09-07',
     @_tarifa_Total_Diaria = 20000,
-    @_deposito_Garantia = 15000,
+    @_deposito_Garantia = 10000,
+	@_costo_alquiler = 20000,
     @_estado_Contrato = 'activo',
     @_Id_cliente = 1,
-	@_codigo_Kit = 2, 
+	@_codigo_Kit = 1, 
 	@_cantidadHerramientasEnKit = 2,
 	@_id_Herramienta = 6
 
