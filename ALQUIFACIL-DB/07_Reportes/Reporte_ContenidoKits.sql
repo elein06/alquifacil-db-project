257980683c39f37df2a82f3c4b89df8dea92d281
Daniel <161152546+SoyMaloLoSeShh@users.noreply.github.com>
2025-06-11 17:34:36 -0600

    Update Reporte_ContenidoKits.sql

--- ALQUIFACIL-DB/07_Reportes/Reporte_ContenidoKits.sql
+++ ALQUIFACIL-DB/07_Reportes/Reporte_ContenidoKits.sql
@@ -1,9 +1,16 @@
-SELECT 
-    KH.id_KitHerramienta,
-    KH.codigo_Kit,
-    KH.Id_Herramienta,
-    KH.cantidad_Herramientas
-FROM 
-    KitHerramienta KH
-WHERE 
-    KH.codigo_Kit = 1;
+CREATE PROCEDURE sp_ObtenerKitHerramientaPorCodigo
+    @CodigoKit INT
+AS
+BEGIN
+    SELECT 
+        KH.id_KitHerramienta,
+        KH.codigo_Kit,
+        KH.Id_Herramienta,
+        KH.cantidad_Herramientas
+    FROM 
+        KitHerramienta KH
+    WHERE 
+        KH.codigo_Kit = @CodigoKit;
+END;
+
+EXEC sp_ObtenerKitHerramientaPorCodigo @CodigoKit = 1;
