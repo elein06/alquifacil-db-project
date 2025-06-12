84ddf0dc8c0921325007dbe38cd2698cf3d94d7f
Andrés Alvarado <andresalvarado080405@gmail.com>
2025-06-12 13:17:52 -0600

    Comentarios eliminados
    Comentarios borrados de la parte de permisos

--- ALQUIFACIL-DB/08_Seguridad/ControlPermisos.sql
+++ ALQUIFACIL-DB/08_Seguridad/ControlPermisos.sql
@@ -1,50 +1,24 @@
 -- ControlPermisos
--- Proyecto ALQUIFÁCIL 
+-- Proyecto ALQUIFÁCIL
 
-
-USE master
-GO
-
-use ALQUIFACIL
+USE MASTER
 GO
 -- RolAdministrativo: Tiene permisos para todo
-/*
-GRANT SELECT, INSERT, UPDATE, DELETE ON Cliente TO RolAdministrativo;
-go
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON Herramienta TO RolAdministrativo;
-go
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON Alquiler TO RolAdministrativo;
-go
-*/
 
 GRANT CONTROL ON DATABASE::ALQUIFACIL TO RolAdministrativo
 GO
 
 
 
+USE ALQUIFACIL
+GO
 -- RolConsulta: Solo puede realizar operaciones de lectura y ejecución de reportes
-/*
-GRANT SELECT ON Cliente TO RolConsulta;
-go
-
-GRANT SELECT ON Herramienta TO RolConsulta;
-go
-
-GRANT SELECT ON Alquiler TO RolConsulta;
-go
-
-GRANT EXECUTE ON sp_verHerramientasRetiradas TO RolConsulta;
-go
-*/
 
 GRANT SELECT ON SCHEMA::dbo TO RolConsulta
 GO
 
 GRANT EXECUTE ON dbo.sp_verClientesAlquilerKitsEspecificos TO RolConsulta;
 GO
-
 
 GRANT EXECUTE ON dbo.sp_verClientesFrecuentes TO RolConsulta;
 GO
@@ -77,24 +51,13 @@ GRANT EXECUTE ON dbo.sp_VerReporteRentabilidadHerramientas TO RolConsulta;
 GO
 
 
--- RolInsercion:Solo puede realizar inserción, pero no actualizar ni borrar
-/*GRANT INSERT ON Cliente TO RolInsercion;
-go
 
-GRANT INSERT ON Herramienta TO RolInsercion;
-go
-
-GRANT INSERT ON Alquiler TO RolInsercion;
-go
-
-
-GRANT EXECUTE TO RolInsercion;
-go
-*/
+USE ALQUIFACIL
+GO
+-- RolInsercion: Solo puede realizar inserción, pero no actualizar ni borrar
 
 GRANT SELECT, INSERT ON SCHEMA::dbo TO RolInsercion
 GO
-
 
 GRANT EXECUTE ON dbo.sp_RegistrarAlquileresConHerramientas TO RolInsercion;
 GO
@@ -123,26 +86,20 @@ GO
 GRANT EXECUTE ON dbo.sp_ingresoTipo TO RolInsercion;
 GO
 
-
 GRANT EXECUTE ON dbo.sp_ingresoCondicionFisica TO RolInsercion;
 GO
-
 
 GRANT EXECUTE ON dbo.sp_ingresoHerramienta TO RolInsercion;
 GO
 
-
 GRANT EXECUTE ON dbo.sp_IngresarKitConHerramientas TO RolInsercion;
 GO
-
 
 GRANT EXECUTE ON dbo.sp_ingresoPersonaResponsable TO RolInsercion;
 GO
 
-
 GRANT EXECUTE ON dbo.sp_InsertarTipoMantenimiento TO RolInsercion;
 GO
-
 
 GRANT EXECUTE ON dbo.sp_ingresoMantenimiento TO RolInsercion;
 GO
